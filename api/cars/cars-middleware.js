cda51d7494c72c74eb7d59537509184942f964fd
Lizzy Thomson <71537606+lizzythomson@users.noreply.github.com>
2022-02-15 13:55:30 -0700

    setup migration, setup cars model file, started middleware

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,15 +1,31 @@
-const checkCarId = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+const carsModel = require('./cars-model');
+
+const checkCarId = async (req, res, next) => {
+  const id = req.params.id;
+  const result = await carsModel.getById(id);
+  if (!result) {
+    res.status(404).json({ message: `car with id ${id} is not found` });
+  } else {
+    req.car = result;
+    next();
+  }
+};
 
 const checkCarPayload = (req, res, next) => {
   // DO YOUR MAGIC
-}
+};
 
 const checkVinNumberValid = (req, res, next) => {
   // DO YOUR MAGIC
-}
+};
 
 const checkVinNumberUnique = (req, res, next) => {
   // DO YOUR MAGIC
-}
+};
+
+module.exports = {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberUnique,
+  checkVinNumberValid,
+};
--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,26 @@
-// DO YOUR MAGIC
+const router = require('express').Router();
+
+const carsModel = require('./cars-model');
+
+const {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberUnique,
+  checkVinNumberValid,
+} = require('./cars-middleware');
+
+router.get('/', (req, res, next) => {
+  carsModel
+    .getAll()
+    .then((cars) => {
+      res.json(cars);
+    })
+    .catch((err) => next(err));
+});
+
+router.use((err, req, res, next) => {
+  console.log('Error', err);
+  res.status(500).json({ message: 'Internal Server Error' });
+});
+
+module.exports = router;
